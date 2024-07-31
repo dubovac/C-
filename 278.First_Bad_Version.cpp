889b5573d05f57ec040decb8bac7dae722a82b8d
dubovac <dubovac@users.noreply.github.com>
2024-07-30 20:24:41 -0500

    Create 278.First_Bad_Version.cpp

---
+++ 278.First_Bad_Version.cpp
@@ -0,0 +1,67 @@
+/**
+ * 278. First Bad Version
+ * Easy
+ * Topics
+ * Companies
+ *
+ * You are a product manager and currently leading a team to develop a new product.
+ * Unfortunately, the latest version of your product fails the quality check.
+ * Since each version is developed based on the previous version, all the versions after a bad version are also bad.
+ *
+ * Suppose you have n versions [1, 2, ..., n] and you want to find out the first bad one, which causes all the following ones to be bad.
+ *
+ * You are given an API bool isBadVersion(version) which returns whether version is bad.
+ * Implement a function to find the first bad version.
+ * You should minimize the number of calls to the API.
+ *
+ *
+ *
+ * Example 1:
+ *
+ * 	Input: n = 5, bad = 4
+ * 	Output: 4
+ * 	Explanation:
+ * 	call isBadVersion(3) -> false
+ * 	call isBadVersion(5) -> true
+ * 	call isBadVersion(4) -> true
+ * 	Then 4 is the first bad version.
+ *
+ * Example 2:
+ *
+ * 	Input: n = 1, bad = 1
+ * 	Output: 1
+ *
+ *
+ *
+ * Constraints:
+ *
+ * 	1 <= bad <= n <= 231 - 1
+ */
+
+// The API isBadVersion is defined for you.
+// bool isBadVersion(int version);
+
+#include <iostream>
+#include <cstdbool>
+
+using namespace std;
+
+bool isBadVersion(int i) {
+	if (i < 2147483647)
+		return false;
+
+	return true;
+}
+
+int firstBadVersion(int n) {
+    for (int i = 1; ; i++)
+        if (isBadVersion(i))
+            return i;
+    n = n;
+
+    return 0;
+}
+
+int main(void) {
+	cout << firstBadVersion(2147483647) << endl;
+}
